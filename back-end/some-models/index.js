22d6563b9c26b41c538e24080319a61161822783
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-09-02 09:08:16 -0300

    Init Sequelize

---
+++ back-end/some-models/index.js
@@ -0,0 +1,11 @@
+const admin = require('./admin');
+const orders = require('./orders');
+const products = require('./products');
+const users = require('./users');
+
+module.exports = {
+  admin,
+  orders,
+  products,
+  users,
+};
